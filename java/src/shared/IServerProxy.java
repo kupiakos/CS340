2dbf245c373605b97d303eb0785ab6136428c32a
kaiboy94 <elijahkai@sbcglobal.net>
2016-09-12 19:57:47 -0600

    created server proxy interface in shared

---
+++ java/src/shared/IServerProxy.java
@@ -0,0 +1,9 @@
+package shared;
+
+/**
+ * Created by elijahgk on 9/12/2016.
+ * Interface to be implemented in ServerProxy and MockProxy classes.
+ * Implements the Catan Server API commands.
+ */
+public interface IServerProxy {
+}
