914338b83ab492025e7f0e02300c2aeb35b89686
kupiakos <kupiakos@gmail.com>
2016-09-21 15:32:51 -0600

    Add sample tests

---
+++ java/tests/shared/facades/ResourcesFacadeTest.java
@@ -0,0 +1,38 @@
+package shared.facades;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ResourcesFacadeTest {
+    @Test
+    public void canPurchaseItem() throws Exception {
+
+    }
+
+    @Test
+    public void purchaseItem() throws Exception {
+
+    }
+
+    @Test
+    public void canReceiveFromBank() throws Exception {
+
+    }
+
+    @Test
+    public void receiveFromBank() throws Exception {
+
+    }
+
+    @Test
+    public void canReturnToBank() throws Exception {
+
+    }
+
+    @Test
+    public void returnToBank() throws Exception {
+
+    }
+
+}
