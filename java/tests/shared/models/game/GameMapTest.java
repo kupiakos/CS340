b707c4d54e24c0e7b233a1f781c7fc728d0e3325
kupiakos <kupiakos@gmail.com>
2016-09-23 15:08:16 -0600

    Add GameMap and ResourceSet test stubs

---
+++ java/tests/shared/models/game/GameMapTest.java
@@ -0,0 +1,84 @@
+package shared.models.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameMapTest {
+
+    @Test
+    public void getPlayerCities() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayerSettlements() throws Exception {
+
+    }
+
+    @Test
+    public void getPlayerRoads() throws Exception {
+
+    }
+
+    @Test
+    public void getHex() throws Exception {
+
+    }
+
+    @Test
+    public void getHexesWithNumber() throws Exception {
+
+    }
+
+    @Test
+    public void getPort() throws Exception {
+
+    }
+
+    @Test
+    public void getRoadOwner() throws Exception {
+
+    }
+
+    @Test
+    public void getSettlementOwner() throws Exception {
+
+    }
+
+    @Test
+    public void getCityOwner() throws Exception {
+
+    }
+
+    @Test
+    public void canAddSettlement() throws Exception {
+
+    }
+
+    @Test
+    public void addSettlement() throws Exception {
+
+    }
+
+    @Test
+    public void canAddRoad() throws Exception {
+
+    }
+
+    @Test
+    public void addRoad() throws Exception {
+
+    }
+
+    @Test
+    public void canUpgradeSettlement() throws Exception {
+
+    }
+
+    @Test
+    public void upgradeSettlement() throws Exception {
+
+    }
+
+}
---
+++ java/tests/shared/models/game/ResourceSetTest.java
@@ -0,0 +1,48 @@
+package shared.models.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ResourceSetTest {
+    @Test
+    public void toNegative() throws Exception {
+
+    }
+
+    @Test
+    public void combine() throws Exception {
+
+    }
+
+    @Test
+    public void subtract() throws Exception {
+
+    }
+
+    @Test
+    public void isSubset() throws Exception {
+
+    }
+
+    @Test
+    public void isNegative() throws Exception {
+
+    }
+
+    @Test
+    public void combine1() throws Exception {
+
+    }
+
+    @Test
+    public void subtract1() throws Exception {
+
+    }
+
+    @Test
+    public void toNegative1() throws Exception {
+
+    }
+
+}
