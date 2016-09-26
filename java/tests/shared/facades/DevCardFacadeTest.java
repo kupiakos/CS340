2562c344753e50e877251e4804323ea649c69863
Philip <philip.horton@att.net>
2016-09-25 21:56:54 -0600

    Committed so that I can merge

---
+++ java/tests/shared/facades/DevCardFacadeTest.java
@@ -0,0 +1,70 @@
+package shared.facades;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Philip on 9/22/2016.
+ */
+public class DevCardFacadeTest {
+    @Test
+    public void canBuyDevCard() throws Exception {
+
+    }
+
+    @Test
+    public void buyDevCard() throws Exception {
+
+    }
+
+    @Test
+    public void canUseDevCard() throws Exception {
+
+    }
+
+    @Test
+    public void useDevCard() throws Exception {
+
+    }
+
+    @Test
+    public void useSoldierCard() throws Exception {
+
+    }
+
+    @Test
+    public void canUseVictoryPointCards() throws Exception {
+
+    }
+
+    @Test
+    public void useVictoryPointCards() throws Exception {
+
+    }
+
+    @Test
+    public void canUseRoadBuildingCard() throws Exception {
+
+    }
+
+    @Test
+    public void useRoadBuildingCard() throws Exception {
+
+    }
+
+    @Test
+    public void canUseYearOfPlentyCard() throws Exception {
+
+    }
+
+    @Test
+    public void useYearOfPlentyCard() throws Exception {
+
+    }
+
+    @Test
+    public void useMonopolyCard() throws Exception {
+
+    }
+}
---
+++ java/tests/shared/facades/RobberFacadeTest.java
@@ -0,0 +1,55 @@
+package shared.facades;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Philip on 9/22/2016.
+ */
+public class RobberFacadeTest {
+    @Test
+    public void callRobberMethodsByRollingA7() throws Exception {
+        //This method should test if the method calls the right methods and in the right order
+        //Call discardHalf successfully
+        //Call moveRobber successfully
+        //Call stealFrom successfully
+    }
+
+    @Test
+    public void callRobberMethodsByAKnightCard() throws Exception {
+        //Same with this method as the first
+        //Call moveRobber successfully
+        //Call stealFrom successfully
+    }
+
+    @Test
+    public void shouldDiscardHalf() throws Exception {
+
+    }
+
+    @Test
+    public void discardHalf() throws Exception {
+
+    }
+
+    @Test
+    public void canMoveRobber() throws Exception {
+
+    }
+
+    @Test
+    public void moveRobber() throws Exception {
+
+    }
+
+    @Test
+    public void canStealFrom() throws Exception {
+
+    }
+
+    @Test
+    public void stealFrom() throws Exception {
+
+    }
+}
