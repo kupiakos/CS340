07e979d23ab77d11e91b61fcf402738d66feb572
Philip <philip.horton@att.net>
2016-09-28 13:57:36 -0600

    RobberFacadeTest

--- java/tests/shared/facades/DevCardFacadeTest.java
+++ java/tests/shared/facades/DevCardFacadeTest.java
@@ -1,6 +1,13 @@
 package shared.facades;
 
+import client.game.GameManager;
 import org.junit.Test;
+import shared.models.game.ClientModel;
+import shared.models.game.Player;
+import shared.models.game.ResourceSet;
+import shared.serialization.ModelExample;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +15,27 @@ import static org.junit.Assert.*;
  * Created by Philip on 9/26/2016.
  */
 public class DevCardFacadeTest {
+
+    private ClientModel model;
+    private Player currentPlayer;
+    private List<Player> listOfPlayers;
+    private DevCardFacade facade;
+
+
+    @org.junit.Before
+    public void setup() {
+        GameManager game = GameManager.getGame();
+        model = ModelExample.fullJsonModel();
+        game.setClientModel(model);
+        facade = game.getFacade().getDevCards();
+        listOfPlayers = model.getPlayers();
+        currentPlayer = listOfPlayers.get(0);
+
+        Player p = listOfPlayers.get(0);
+        ResourceSet resourceSet = new ResourceSet(1, 0, 1, 0, 1);
+        p.setResources(resourceSet);
+    }
+
     @Test
     public void canBuyDevCard() throws Exception {
 
