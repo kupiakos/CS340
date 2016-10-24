ee7ee1054be1d2ca4f1f0b41318c48912faf8883
Sarah <sarahkatherine@gmail.com>
2016-10-23 20:55:27 -0600

    update ResourceBarController updateFromModel and PointsController

--- java/src/client/points/PointsController.java
+++ java/src/client/points/PointsController.java
@@ -1,7 +1,9 @@
 package client.points;
 
 import client.base.Controller;
+import shared.definitions.PlayerIndex;
 import shared.models.game.ClientModel;
+import shared.models.game.Player;
 
 
 /**
@@ -18,21 +20,34 @@ public class PointsController extends Controller implements IPointsController {
      * @param finishedView Game finished view, which is displayed when the game is over
      */
     public PointsController(IPointsView view, IGameFinishedView finishedView) {
-
         super(view);
-
         setFinishedView(finishedView);
-
         initFromModel();
+        observeClientModel();
+    }
+
+    private void initFromModel() {
+        updateFromModel(getModel());
     }
 
     @Override
     protected void updateFromModel(ClientModel model) {
-        // Do nothing by default, overridden by classes that need it
+        Player player = getGameManager().getPlayer();
+        int points = player.getVictoryPoints();
+        getPointsView().setPoints(points);
+
+        PlayerIndex winnerIndex = model.getWinner();
+        if (winnerIndex.index() != -1) {
+            Player winner = model.getPlayer(winnerIndex);
+            boolean isLocalPlayer = false;
+            if (player.equals(winner)) {
+                isLocalPlayer = true;
+            }
+            getFinishedView().setWinner(winner.getName(), isLocalPlayer);
+        }
     }
 
     public IPointsView getPointsView() {
-
         return (IPointsView) super.getView();
     }
 
@@ -42,12 +57,6 @@ public class PointsController extends Controller implements IPointsController {
 
     public void setFinishedView(IGameFinishedView finishedView) {
         this.finishedView = finishedView;
-    }
-
-    private void initFromModel() {
-        //<temp>
-        getPointsView().setPoints(5);
-        //</temp>
     }
 
 }
--- java/src/client/resources/ResourceBarController.java
+++ java/src/client/resources/ResourceBarController.java
@@ -2,7 +2,11 @@ package client.resources;
 
 import client.base.Controller;
 import client.base.IAction;
+import shared.definitions.PlayerIndex;
+import shared.definitions.PurchaseType;
+import shared.facades.ResourcesFacade;
 import shared.models.game.ClientModel;
+import shared.models.game.Player;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,6 +46,10 @@ public class ResourceBarController extends Controller implements IResourceBarCon
         elementActions.put(element, action);
     }
 
+    /**
+     * Called when the user presses the enabled build road button.
+     * Activates the modal to display, so that it can then build a road
+     */
     @Override
     public void buildRoad() {
         executeElementAction(ResourceBarElement.ROAD);
@@ -53,7 +61,6 @@ public class ResourceBarController extends Controller implements IResourceBarCon
      */
     @Override
     public void buildSettlement() {
-
         executeElementAction(ResourceBarElement.SETTLEMENT);
     }
 
@@ -63,10 +70,13 @@ public class ResourceBarController extends Controller implements IResourceBarCon
      */
     @Override
     public void buildCity() {
-
         executeElementAction(ResourceBarElement.CITY);
     }
 
+    /**
+     * Called when the user presses the enabled buy card button.
+     * Activates the modal to display, so that it can then buy a card
+     */
     @Override
     public void buyCard() {
         executeElementAction(ResourceBarElement.BUY_CARD);
@@ -96,43 +106,45 @@ public class ResourceBarController extends Controller implements IResourceBarCon
     @Override
     public void updateFromModel(ClientModel model) {
 
-//        Player player = ;
-//        DevCardFacade devCardFacade = getFacade().getDevCards();
-//        ResourcesFacade resourcesFacade = getFacade().getResources();
-//
-//        int brick = player.getResources().getBrick();
-//        getView().setElementAmount(ResourceBarElement.BRICK, brick);
-//        int ore = player.getResources().getOre();
-//        getView().setElementAmount(ResourceBarElement.ORE, ore);
-//        int sheep = player.getResources().getSheep();
-//        getView().setElementAmount(ResourceBarElement.SHEEP, sheep);
-//        int wood = player.getResources().getWood();
-//        getView().setElementAmount(ResourceBarElement.WOOD, wood);
-//        int wheat = player.getResources().getWheat();
-//        getView().setElementAmount(ResourceBarElement.WHEAT, wheat);
-//
-//        int roads = player.getRoads();
-//        getView().setElementAmount(ResourceBarElement.ROAD, roads);
-//        getView().setElementEnabled(ResourceBarElement.ROAD, resourcesFacade.canPurchaseItem(player, PurchaseType.ROAD));
-//
-//        int settlements = player.getSettlements();
-//		getView().setElementAmount(ResourceBarElement.SETTLEMENT, settlements);
-//        getView().setElementEnabled(ResourceBarElement.SETTLEMENT, resourcesFacade.canPurchaseItem(player, PurchaseType.SETTLEMENT));
-//
-//		int cities = player.getCities();
-//		getView().setElementAmount(ResourceBarElement.CITY, cities);
-//        getView().setElementEnabled(ResourceBarElement.CITY, resourcesFacade.canPurchaseItem(player, PurchaseType.CITY));
+        Player player = getGameManager().getPlayer();
+        ResourcesFacade resourcesFacade = getFacade().getResources();
 
-//        if(its the players turn){
-//			getView().setElementEnabled(ResourceBarElement.PLAY_CARD, true);
-//		}
-//		else {
-//			getView().setElementEnabled(ResourceBarElement.PLAY_CARD, false);
-//		}
-//		//get old or new dev cards?
-//		int roadBuilder = player.getNewDevCards().getRoadBuilding();
-//		getView().setElementAmount(ResourceBarElement.ROAD, roadBuilder);
-//        getView().setElementEnabled(ResourceBarElement.CITY, resourcesFacade.canPurchaseItem(player, PurchaseType.CITY));
+        int brick = player.getResources().getBrick();
+        getView().setElementAmount(ResourceBarElement.BRICK, brick);
+        int ore = player.getResources().getOre();
+        getView().setElementAmount(ResourceBarElement.ORE, ore);
+        int sheep = player.getResources().getSheep();
+        getView().setElementAmount(ResourceBarElement.SHEEP, sheep);
+        int wood = player.getResources().getWood();
+        getView().setElementAmount(ResourceBarElement.WOOD, wood);
+        int wheat = player.getResources().getWheat();
+        getView().setElementAmount(ResourceBarElement.WHEAT, wheat);
+
+        int roads = player.getRoads();
+        getView().setElementAmount(ResourceBarElement.ROAD, roads);
+        getView().setElementEnabled(ResourceBarElement.ROAD, resourcesFacade.canPurchaseItem(player, PurchaseType.ROAD));
+
+        int settlements = player.getSettlements();
+        getView().setElementAmount(ResourceBarElement.SETTLEMENT, settlements);
+        getView().setElementEnabled(ResourceBarElement.SETTLEMENT, resourcesFacade.canPurchaseItem(player, PurchaseType.SETTLEMENT));
+
+        int cities = player.getCities();
+        getView().setElementAmount(ResourceBarElement.CITY, cities);
+        getView().setElementEnabled(ResourceBarElement.CITY, resourcesFacade.canPurchaseItem(player, PurchaseType.CITY));
+
+        PlayerIndex currentPlayerIndex = model.getTurnTracker().getCurrentTurn();
+        Player currentPlayer = model.getPlayer(currentPlayerIndex);
+
+        if (player.equals(currentPlayer)) {
+            getView().setElementEnabled(ResourceBarElement.BUY_CARD, true);
+            getView().setElementEnabled(ResourceBarElement.PLAY_CARD, true);
+        } else {
+            getView().setElementEnabled(ResourceBarElement.BUY_CARD, false);
+            getView().setElementEnabled(ResourceBarElement.PLAY_CARD, false);
+        }
+
+        int soldiers = player.getSoldiers();
+        getView().setElementAmount(ResourceBarElement.SOLDIERS, soldiers);
 
     }
 }
