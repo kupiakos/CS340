b1840c3c40f80119a735889435ebf137c8fd544e
kupiakos <kupiakos@gmail.com>
2016-11-07 13:03:16 -0700

    Upgrade User and change ServerModel

--- java/src/server/models/ServerModel.java
+++ java/src/server/models/ServerModel.java
@@ -1,38 +1,58 @@
 package server.models;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import javax.security.auth.login.CredentialNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerModel {
 
-    private Map<Integer, User> idToUser;
-    private Map<Integer, GameModel> gameIDToModel;
+    // Possibly consider a Map<String, User> instead
+    private Map<Integer, User> users;
+    private Map<Integer, GameModel> gameModels;
 
     public ServerModel() {
-        idToUser = new HashMap<>();
-        gameIDToModel = new HashMap<>();
+        users = new HashMap<>();
+        gameModels = new HashMap<>();
     }
 
     /**
-     * Called when a new {@link User} registers.  This method inserts a
-     * new {@link User} into {@link ServerModel#idToUser}
+     * Register a new user with a given username and password.
      *
-     * @pre {@link #getUser(int)} returns a valid {@link User} for some {@code id}
-     * @post A new {@link User} is placed in {@link #idToUser}
+     * @return the ID of the new user
      */
-    public void registerUser() {
-
+    public int registerUser(@NotNull String username, @NotNull String password) throws CredentialNotFoundException {
+        if (users.values().stream().anyMatch(u -> username.equals(u.getUsername()))) {
+            throw new CredentialNotFoundException("The user " + username + " already exists!");
+        }
+        int id = users.keySet().stream().max(Integer::compareTo).orElseGet(() -> 0) + 1;
+        User user = new User();
+        user.setId(id);
+        assert !users.containsKey(id);
+        users.put(id, user);
+        return id;
     }
 
     /**
-     * Called when a new game is created.  This method creates a new {@link GameModel}
-     * and inserts it into {@link ServerModel#gameIDToModel}
+     * Determine whether a username and password combination are already registered
+     *
+     * @param username the username, not null
+     * @param password the password, not null
+     * @return true if the user is authenticated, else false
+     */
+    public boolean authenticateUser(@NotNull String username, @NotNull String password) {
+        User user = users.values().stream().filter(u -> username.equals(u.getUsername())).findFirst().orElse(null);
+        return user != null && user.getPassword().equals(password);
+    }
+
+    /**
+     * Called when a new game is created.  This method creates a new {@link GameModel}.
      *
      * @return the new {@link GameModel#id} created
      * @pre {@link #getGameModel(int)} returns a valid {@link GameModel} for some {@code id}
-     * @post A new {@link GameModel} is placed in {@link #gameIDToModel}
+     * @post A new {@link GameModel} is placed in {@link #gameModels}
      */
     public int startNewGame() {
         return 0;
@@ -42,25 +62,27 @@ public class ServerModel {
      * Returns a {@link User} object or null
      *
      * @param ID An integer that is used to determine which {@link User} to return
-     * @return a {@link User} if {@code ID} is contained in {@link #idToUser},
+     * @return a {@link User} if {@code ID} is contained in {@link #users},
      * null otherwise
      * @pre None
      * @post A {@link User} object or null is returned
      */
+    @Nullable
     public User getUser(int ID) {
-        return idToUser.get(ID);
+        return users.get(ID);
     }
 
     /**
      * Returns a {@link GameModel} object or null
      *
      * @param ID An integer that is used to determine which {@link GameModel} to return
-     * @return a {@link GameModel} if {@code ID} is contained in {@link #gameIDToModel},
+     * @return a {@link GameModel} if {@code ID} is contained in {@link #gameModels},
      * null otherwise
      * @pre None
      * @post A value {@link GameModel} object or null is returned
      */
+    @Nullable
     public GameModel getGameModel(int ID) {
-        return gameIDToModel.get(ID);
+        return gameModels.get(ID);
     }
 }
