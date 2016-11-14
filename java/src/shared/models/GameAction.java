7c1bafbaabb14cd26a97b3c644e9c952ea8b6d90
kupiakos <kupiakos@gmail.com>
2016-11-14 12:21:03 -0700

    Implement GameAction

--- java/src/shared/models/GameAction.java
+++ java/src/shared/models/GameAction.java
@@ -1,9 +1,24 @@
 package shared.models;
 
+import shared.facades.FacadeManager;
+import shared.models.game.ClientModel;
+
 /**
  * Created by elijahgk on 11/6/2016.
  * IGameAction is the interface for all game actions in Catan.  Think of a Game Action as a Command.
  */
 public abstract class GameAction implements ICommandAction {
+    private FacadeManager facades;
 
+    public FacadeManager getFacades() {
+        return facades;
+    }
+
+    public void setFacades(FacadeManager facades) {
+        this.facades = facades;
+    }
+
+    protected ClientModel getModel() {
+        return getFacades().getClientModel();
+    }
 }
