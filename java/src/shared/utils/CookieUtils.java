7d744b4a2104073752e70a29a2302dc20d9f2ec1
kupiakos <kupiakos@gmail.com>
2016-11-14 16:10:49 -0700

    Fix cookies

---
+++ java/src/shared/utils/CookieUtils.java
@@ -0,0 +1,28 @@
+package shared.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.HttpCookie;
+import java.net.URLDecoder;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CookieUtils {
+    public static String decodeCookie(HttpCookie c) {
+        try {
+            return URLDecoder.decode(c.getValue(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static Map<String, String> getCookieMap(Stream<HttpCookie> cookies) {
+        return cookies.collect(Collectors.toMap(HttpCookie::getName, CookieUtils::decodeCookie));
+    }
+
+    public static Map<String, String> getCookieMap(Collection<HttpCookie> cookies) {
+        return getCookieMap(cookies.stream());
+    }
+}
