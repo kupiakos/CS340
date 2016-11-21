d4b9ed5068cea27b35eba6aae3906eb2434787e3
Austin Harrison <austin.harrison@byu.net>
2016-11-20 21:31:10 -0700

    moved SwaggerHandler to own class, removed commons.io

--- java/src/server/client/ServerCommunicator.java
+++ java/src/server/client/ServerCommunicator.java
@@ -8,16 +8,10 @@ import org.jetbrains.annotations.Nullable;
 import server.games.IServerManager;
 import shared.IServer;
 import shared.annotations.ServerEndpoint;
-import shared.utils.FileUtils;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -127,99 +121,5 @@ ServerCommunicator implements HttpHandler, IServerCommunicator {
         }
 
         swaggerHandler.setServerManager(serverManager);
-    }
-
-    // *************************
-    // Handlers below
-    // *************************
-
-    private class EndpointHandler implements HttpHandler {
-        private EndpointDispatcher getMethod, postMethod;
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            switch (exchange.getRequestMethod().toUpperCase()) {
-                case "GET":
-                    if (getMethod != null) {
-                        getMethod.handle(exchange);
-                        return;
-                    }
-                    break;
-                case "POST":
-                    if (postMethod != null) {
-                        postMethod.handle(exchange);
-                        return;
-                    }
-                    break;
-            }
-            // Method not allowed
-            sendResponse(exchange, 405, null);
-        }
-
-        void setServerManager(IServerManager serverManager) {
-            if (getMethod != null) {
-                getMethod.setServerManager(serverManager);
-            }
-            if (postMethod != null) {
-                postMethod.setServerManager(serverManager);
-            }
-        }
-
-        void setGetMethod(EndpointDispatcher getMethod) {
-            this.getMethod = getMethod;
-        }
-
-        void setPostMethod(EndpointDispatcher postMethod) {
-            this.postMethod = postMethod;
-        }
-    }
-
-    /**
-     * Handles all the html and json requests to run swagger
-     * Appends ".json" to the request before getting the proper file from the file system
-     */
-    private class SwaggerHandler extends EndpointHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-
-            if (exchange.getRequestMethod().toUpperCase().equals("GET")) {
-                String uri = exchange.getRequestURI().toString();
-
-                String filepath = System.getProperty("user.dir") +
-                        exchange.getRequestURI().toString() +
-                        ((uri.contains("data")) ? ".json" : "");
-
-                LOGGER.warning("GET Swagger: " + filepath);
-
-
-                byte[] encoded = Files.readAllBytes(Paths.get(filepath));
-
-                ArrayList<String> mimetypes = new ArrayList<>();
-                mimetypes.add(FileUtils.getMimeType(filepath));
-                exchange.getResponseHeaders().add("Content-Type", mimetypes.get(0));
-                exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport, *");
-                exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "POST, GET, OPTIONS , PUT");
-                exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-                exchange.getResponseHeaders().add("Access-Control-Request-Headers", "Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport,  *");
-
-                exchange.sendResponseHeaders(200, encoded.length);
-
-                try (BufferedOutputStream out = new BufferedOutputStream(exchange.getResponseBody())) {
-                    try (ByteArrayInputStream bis = new ByteArrayInputStream(encoded)) {
-                        byte[] buffer = new byte[1024];
-                        int count;
-                        while ((count = bis.read(buffer)) != -1) {
-                            out.write(buffer, 0, count);
-                        }
-                    }
-                } catch (Exception e) {
-                    LOGGER.warning("GET Swagger: " + uri);
-                    e.printStackTrace();
-                }
-            } else {
-                // Method not allowed
-                sendResponse(exchange, 405, null);
-            }
-        }
     }
 }
