758a1aae3842ea7c73fdc1a1ee7509a0d7fa2e5c
kaiboy94 <elijahkai@sbcglobal.net>
2016-12-02 10:45:53 -0700

    setting up db stuff

---
+++ java/src/server/db/IDAO.java
@@ -0,0 +1,20 @@
+package server.db;
+
+import java.util.List;
+
+/**
+ * Created by elija on 12/2/2016.
+ */
+public interface IDAO<T> {
+
+    T findById(int id);
+
+    List<T> findAll();
+
+    boolean insert(T obj);
+
+    boolean update(T obj);
+
+    boolean delete(T obj);
+
+}
