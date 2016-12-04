0a33ebe9e6e5365ccb89976a5c40ef2b0a137450
kaiboy94 <elijahkai@sbcglobal.net>
2016-12-04 10:14:34 -0700

    Implemented Postgres UserDAO

--- java/src/server/db/postgres/PostgresTest.java
+++ java/src/server/db/postgres/PostgresTest.java
@@ -2,6 +2,9 @@ package server.db.postgres;
 
 import org.junit.Before;
 import org.junit.Test;
+import server.models.User;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by elija on 12/3/2016.
@@ -15,10 +18,21 @@ public class PostgresTest {
     }
 
     @Test
-    public void test() {
-        pp.startTransaction();
-        pp.finishTransaction();
-        pp.clearDB();
+    public void testUserDAO() {
+        assertTrue(pp.startTransaction());
+        assertTrue(pp.finishTransaction());
+        assertTrue(pp.clearDB());
+        assertTrue(pp.getUserDAO().insert(new User(1, "James", "password")));
+        assertTrue(pp.getUserDAO().update(new User(1, "Bob", "password")));
+        assertTrue(pp.getUserDAO().findById(1) != null);
+        assertTrue(pp.getUserDAO().delete(new User(1, "whatever", "whocares")));
+        assertTrue(pp.getUserDAO().insert(new User(1, "James", "password")));
+        assertTrue(pp.getUserDAO().insert(new User(2, "Bob", "password")));
+        assertTrue(pp.getUserDAO().insert(new User(3, "Kevin", "password")));
+        assertTrue(pp.getUserDAO().findAll().size() == 3);
+
+
+
     }
 
 }
