0fcf0073b05f4cf79dca53c6475b4b1451c33099
kaiboy94 <elijahkai@sbcglobal.net>
2016-12-07 20:09:22 -0700

    postgres done (#106)
    * update games after executing commands at startup
    
    * postgres done (hopefully)
    
    * update games after executing commands at startup
    
    * postgres done (hopefully)
    
    * update games after executing commands at startup
    
    * postgres done (hopefully)

--- java/src/server/games/ServerManager.java
+++ java/src/server/games/ServerManager.java
@@ -124,9 +124,11 @@ public class ServerManager implements IServerManager {
                 action.execute();
             } else {
                 GameAction action = (GameAction) command;
-                action.setFacades(((GameServer) runningServers.get(command.getGameId())).getFacades());
+                action.setFacades(((GameServer) getGameServer(command.getGameId())).getFacades());
                 action.execute();
             }
+            persistenceProvider.getGameDAO().clearCommands();
+            getServerModel().updateGamesInDatabase(persistenceProvider);
         }
     }
 }
